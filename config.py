1c23f8025a7cee6a629e6d104d2ffe5535e61fd1
shkabdullah0985-cmyk <shkabdullah0985@gmail.com>
2026-07-23 20:36:15 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '@bdullah1266'   # change to your password
-    MYSQL_DB = 'quastech_db'
